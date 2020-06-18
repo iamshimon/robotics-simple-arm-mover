1dfe6a10e304456038e2009e7bfaad26bed8029c
Shimon Payyanadan <shimonpmohanan@gmail.com>
2020-06-18 22:17:49 +0530

    uploading the project for reference

---
+++ simple_arm/src/simple_mover_class.cpp
@@ -0,0 +1,65 @@
+#include <ros/ros.h>
+#include <std_msgs/Float64.h>
+
+// Implementing simple_mover node in Pub-Sub Class template
+
+class SimpleMover
+{
+    public:
+        SimpleMover()
+        {
+            joint1_pub = n_.advertise<std_msgs::Float64>("/simple_arm/joint_1_position_controller/command", 10);
+            joint2_pub = n_.advertise<std_msgs::Float64>("/simple_arm/joint_2_position_controller/command", 10);
+        
+        }
+
+        bool PublishJointAngles()
+        {
+                  
+                            // Set loop frequency of 10Hz
+                ros::Rate loop_rate(10);
+
+                int start_time, elapsed;
+
+                // Get ROS start time
+                while (not start_time) {
+                    start_time = ros::Time::now().toSec();
+                }
+
+                while (ros::ok()) {
+                    // Get ROS elapsed time
+                    elapsed = ros::Time::now().toSec() - start_time;
+
+                    // Set the arm joint angles
+                    std_msgs::Float64 joint1_angle, joint2_angle;
+                    joint1_angle.data = sin(2 * M_PI * 0.1 * elapsed) * (M_PI / 2);
+                    joint2_angle.data = sin(2 * M_PI * 0.1 * elapsed) * (M_PI / 2);
+
+                    // Publish the arm joint angles
+                    joint1_pub.publish(joint1_angle);
+                    joint2_pub.publish(joint2_angle);
+
+                    // Sleep for the time remaining until 10 Hz is reached
+                    loop_rate.sleep();
+                }
+            return true;
+        }
+
+    private:
+        ros::NodeHandle n_;
+        ros::Publisher joint1_pub;
+        ros::Publisher joint2_pub;
+    
+};
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "arm_mover");
+
+    SimpleMover SM;
+    SM.PublishJointAngles();
+
+    return 0;
+
+    
+}
